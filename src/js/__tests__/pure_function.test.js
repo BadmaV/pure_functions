c0c071967eddeb47e97aed1277e951660c61d5a5
Badma <andraev@ya.ru>
2020-04-03 23:46:31 +0300

    jest coverage

--- src/js/__tests__/pure_function.test.js
+++ src/js/__tests__/pure_function.test.js
@@ -1,7 +1,7 @@
 import calculateHealth from '../pure_function';
 
 test('character health testing', () => {
-  const input = {name: 'Leonardo', health: 90};
+  const input = { name: 'Leonardo', health: 90 };
 
   const expected = 'healthy';
   const received = calculateHealth(input);
@@ -9,15 +9,15 @@ test('character health testing', () => {
 });
 
 test('character health testing', () => {
-  const input = {name: 'rafael', health: 40};
+  const input = { name: 'rafael', health: 40 };
 
   const expected = 'wounded';
   const received = calculateHealth(input);
   expect(received).toBe(expected);
 });
 
-test ('character health testing', () => {
-  const input = {name: 'donatello', health: 10};
+test('character health testing', () => {
+  const input = { name: 'donatello', health: 10 };
 
   const expected = 'critical';
   const received = calculateHealth(input);
@@ -25,7 +25,7 @@ test ('character health testing', () => {
 });
 
 test('test', () => {
-  const input = {name: 'miki', health: 'name'};
+  const input = { name: 'miki', health: 'name' };
 
   const expected = 'not value';
 
